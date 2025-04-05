ea40e2bebb904008cd2712fd4d11fdc54a0763f2
k0smaX <kostylev.mack@yandex.ru>
2025-04-05 23:21:57 +0300

    update Category and Product class

--- src/category.py
+++ src/category.py
@@ -15,6 +15,24 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """
+        Метод для добавления товаров
+        :param product: товар
+        :return: None
+        """
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """ Геттер для вывода списка товаров в виде строк """
+        list_product = []
+        for product in self.__products:
+            list_product.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+
+        return "\n".join(list_product)
